c4ba668c4b1e11244ed9fba4eee8a866512f71e8
richjfly@gmail.com <richjfly@gmail.com>
2026-05-16 18:48:53 +0000

    stg models for __ref modularity

--- models/customers.sql
+++ models/customers.sql
@@ -1,56 +1,48 @@
-with customers as (
+with
+    customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
+        select *
+        from {{ ref('stg_jaffle_shop__customers') }}
 
-    from raw.jaffle_shop.customers
+    ),
 
-),
+    orders as (
 
-orders as (
+        select *
+        from {{ ref('stg_jaffle_shop__orders') }}
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
+    ),
 
-    from raw.jaffle_shop.orders
+    customer_orders as (
 
-),
+        select
+            customer_id,
 
-customer_orders as (
+            min(order_date) as first_order_date,
+            max(order_date) as most_recent_order_date,
+            count(order_id) as number_of_orders
 
-    select
-        customer_id,
+        from orders
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        group by 1
 
-    from orders
+    ),
 
-    group by 1
+    final as (
 
-),
+        select
+            customers.customer_id,
+            customers.first_name,
+            customers.last_name,
+            customer_orders.first_order_date,
+            customer_orders.most_recent_order_date,
+            coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
+        from customers
 
-final as (
+        left join customer_orders using (customer_id)
 
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+    )
 
-    from customers
-
-    left join customer_orders using (customer_id)
-
-)
-
-select * from final
+select *
+from final
