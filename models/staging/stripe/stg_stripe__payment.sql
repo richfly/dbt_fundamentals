509718c845d8cc31bad074a080dba0352a12e452
richjfly@gmail.com <richjfly@gmail.com>
2026-05-17 19:17:46 +0000

    update stg_stripe

--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -1,24 +1,9 @@
-with 
-
-source as (
-
-    select * from {{ source('stripe', 'payment') }}
-
-),
-
-renamed as (
-
-    select
-        id as payment_id,
-        orderid as order_id,
-        paymentmethod as payment_method,
-        status as payment_status,
-        amount as payment_amount,
-        created as payment_created,
-        _batched_at
-
-    from source
-
-)
-
-select * from renamed
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+from {{ source('stripe', 'payment') }}
