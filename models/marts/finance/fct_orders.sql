50ac2846a5d310182371dec9a0df1c03e1ff87af
richjfly@gmail.com <richjfly@gmail.com>
2026-05-17 18:35:18 +0000

    update fct_orders

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,5 +1,30 @@
-select
-    ORDERID as order_id
-    , id as customer_id
-    , AMOUNT
-from stg_stripe__payments
+with orders as  (
+    select * from {{ ref ('stg_jaffle_shop__orders' )}}
+),
+
+payments as (
+    select * from {{ ref ('stg_stripe__payment') }}
+),
+
+order_payments as (
+    select
+        order_id,
+        sum (case when payment_status = 'success' then payment_amount end) as amount
+
+    from payments
+    group by 1
+),
+
+ final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce (order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
+)
+
+select * from final
