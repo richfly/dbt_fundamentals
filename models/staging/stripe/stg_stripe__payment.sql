70f64929c757b53298c0f3e2b6853cb10fff62aa
richjfly@gmail.com <richjfly@gmail.com>
2026-05-20 02:39:12 +0000

    add relationships test

--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -2,8 +2,8 @@ select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
-    status,
-    -- amount is stored in cents, convert it to dollars
+    status  as payment_status,
+    amount as payment_amount,
     amount / 100 as amount,
     created as created_at
 from {{ source('stripe', 'payment') }}
